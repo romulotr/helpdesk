f4ddcb3c92f30c9652013ab31764d12aad8b789b
Rômulo Troian <7254854+romulotr@users.noreply.github.com>
2022-01-20 22:06:26 -0300

    update
    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,11 +9,44 @@ module.exports = {
 		},
 	},
 	plugins: [
-		'gatsby-plugin-postcss',
-		`gatsby-plugin-image`,
-		`gatsby-plugin-sharp`,
-		`gatsby-transformer-sharp`,
-		`gatsby-plugin-react-helmet`,
+		{
+		resolve: `gatsby-plugin-google-gtag`,
+		options: {
+			// You can add multiple tracking ids and a pageview event will be fired for all of them.
+			trackingIds: [
+			"G-TDSD8782RG"
+			],
+			// This object is used for configuration specific to this plugin
+			pluginConfig: {
+			// Puts tracking script in the head instead of the body
+			head: true,
+			// Setting this parameter is also optional
+			respectDNT: true
+			},
+		},
+		},
+		{
+		resolve: `gatsby-plugin-manifest`,
+		options: {
+			name: `Base de Conhecimento - wEstoque.com.br`,
+			short_name: `Ajuda wEstoque`,
+			background_color: `#f1f6fa`,
+			theme_color: `#232323`,
+			display: "standalone",
+			start_url: "/",
+			icon: `src/images/favicon.png`,
+			cache_busting_mode: 'none',
+		}
+		},
+		{
+		resolve: `gatsby-plugin-offline`,
+		options: {			  
+			workboxConfig: {
+			globPatterns: ['**/*.{js,jpg,png,html,css}']
+			},
+			precachePages: [`/`, `/chamado`, `/404`, `/index`, `/*`],
+		}
+		},
 		{
 		resolve: 'gatsby-plugin-robots-txt',
 		options: {
@@ -21,6 +54,11 @@ module.exports = {
 			policy: [{ userAgent: '*', allow: '/' }]
 			}
 		},
+		'gatsby-plugin-postcss',
+		`gatsby-plugin-image`,
+		`gatsby-plugin-sharp`,
+		`gatsby-transformer-sharp`,
+		`gatsby-plugin-react-helmet`,
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
@@ -38,56 +76,16 @@ module.exports = {
 		{
 			resolve: `gatsby-transformer-remark`,
 			options: {
-				// CommonMark mode (default: true)
-				commonmark: true,
 				// Footnotes mode (default: true)
 				footnotes: true,
-				// Pedantic mode (default: true)
-				pedantic: true,
 				// GitHub Flavored Markdown mode (default: true)
 				gfm: true,
 				// Plugins configs
 				plugins: [],
 			},
 		},
-		{
-			resolve: `gatsby-plugin-manifest`,
-			options: {
-			  name: `Base de Conhecimento - wEstoque.com.br`,
-			  short_name: `Ajuda wEstoque`,
-			  background_color: `#f1f6fa`,
-			  theme_color: `#232323`,
-			  display: "standalone",
-			  start_url: "/",
-			  icon: `src/images/favicon.png`,
-			  cache_busting_mode: 'none',
-			}
-		  },
-		  {
-			resolve: `gatsby-plugin-offline`,
-			options: {			  
-			  workboxConfig: {
-				globPatterns: ['**/*.{js,jpg,png,html,css}']
-			  },
-			  precachePages: [`/`, `/chamado`, `/404`, `/index`, `/*`],
-			}
-		  },
-		  {
-			resolve: `gatsby-plugin-google-gtag`,
-			options: {
-				// You can add multiple tracking ids and a pageview event will be fired for all of them.
-				trackingIds: [
-				"G-TDSD8782RG"
-				],
-				// This object is used for configuration specific to this plugin
-				pluginConfig: {
-				// Puts tracking script in the head instead of the body
-				head: true,
-				// Setting this parameter is also optional
-				respectDNT: true
-				},
-			},
-		  },
+		
+		  
 
 	],
 }
